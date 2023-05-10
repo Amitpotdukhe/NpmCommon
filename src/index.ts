0d2b8037f7eeb5c8150e5cdc8b2a3954fa5f22b8
Amitpotdukhe <69644033+Amitpotdukhe@users.noreply.github.com>
2023-05-10 15:13:01 +0530

    config

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,16 @@
-interface Color {
-    red: number;
-    blue: number;
-    green: number;
-}
+export * from './errors/bad-request-error'
+export * from './errors/custom-error'
+export * from './errors/database-connection-error'
+export * from './errors/not-authorized-error'
+export * from './errors/not-found-error'
+export * from './errors/request-validation-error'
 
-const color : Color = {
-    red:10,
-    blue:10,
-    green:10
-}
 
-console.log(color);
+export * from './middlewares/current-user'
+export * from './middlewares/error-handler'
+export * from './middlewares/require-auth'
+export * from './middlewares/request-validation'
 
-export default color
+
+
+// git commit & push npm version patch || npm run build || npm publish
